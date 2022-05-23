edd92d9bee22d66b1083642314417a3f9017e3b6
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-05-22 23:45:54 -0700

    update

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -1,5 +1,6 @@
 def recursive_binary_search(list, target):
-    if len(list) == 0:  # this will check to see if list is empty and return false
+    # this will check to see if list is empty and return false.
+    if len(list) == 0:
         return False
     else:
         midpoint = (len(list))//2
